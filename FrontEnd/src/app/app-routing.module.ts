1651a3327f12650236689c6ff3d1a64d876b4fde
QV NGUYEN <qv.nguyen00@gmail.com>
2023-03-24 10:56:27 +0100

    add routing

--- FrontEnd/src/app/app-routing.module.ts
+++ FrontEnd/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component'
+import { HostsComponent } from './hosts/hosts.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomepageComponent },
+  { path: 'hosts', component: HostsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
