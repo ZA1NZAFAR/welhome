e711001ed52c5ba229fe3ff13425ed9c9154a3a4
QV NGUYEN <qv.nguyen00@gmail.com>
2023-03-24 10:54:52 +0100

    add footer and navbar

--- FrontEnd/src/app/app.module.ts
+++ FrontEnd/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FooterComponent } from './commons/footer/footer.component';
+import { NavbarComponent } from './commons/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
