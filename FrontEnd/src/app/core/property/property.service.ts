2f19e1bf66cd11a7ca9ddc2a962a1b6f96652f88
QV NGUYEN <qv.nguyen00@gmail.com>
2023-03-30 11:27:21 +0200

    simple layout for cards

--- FrontEnd/src/app/core/property/property.service.ts
+++ FrontEnd/src/app/core/property/property.service.ts
@@ -16,8 +16,24 @@ const mockProperties: IProperty[] = [
     price: 100,
     surface_area: 100,
     floors: 1,
+    capacity: 4,
+    owner_email: 'zain.zafar@gmail.com',
+    image_url: 'https://cdn.pixabay.com/photo/2016/11/18/17/46/house-1836070_960_720.jpg'
+  },
+  {
+    title: 'Room in Toulouse',
+    description: 'A beautiful room in Toulouse',
+    property_catergory: 'House',
+    address: '1, rue de la Republique',
+    city: 'Toulouse',
+    state: 'Occitanie',
+    country: 'France',
+    price: 100,
+    surface_area: 100,
+    floors: 5,
     capacity: 2,
     owner_email: 'zain.zafar@gmail.com',
+    image_url: 'https://images.unsplash.com/photo-1615874959474-d609969a20ed?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVkJTIwcm9vbXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60'
   },
   {
     title: 'Apartment in Lyon',
@@ -31,7 +47,8 @@ const mockProperties: IProperty[] = [
     surface_area: 50,
     floors: 5,
     capacity: 4,
-    owner_email: 'abc@gmail.com'
+    owner_email: 'abc@gmail.com',
+    image_url: 'https://media.istockphoto.com/id/1165384568/fr/photo/complexe-moderne-europ%C3%A9en-de-b%C3%A2timents-r%C3%A9sidentiels.jpg?s=612x612&w=0&k=20&c=nvoIbiIffCt-nuj47Cc3I261Ke98iMouq_HefNM7Lz0='
   }
 ];
 
