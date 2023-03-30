1172e4be509401faba7464abf36e076c9acb7bf0
QV NGUYEN <qv.nguyen00@gmail.com>
2023-03-30 11:02:43 +0200

    refactor mock data

--- FrontEnd/src/app/core/property/property.service.ts
+++ FrontEnd/src/app/core/property/property.service.ts
@@ -2,24 +2,45 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { IProperty } from './property.model';
 import { BehaviorSubject, Subject } from 'rxjs';
+import { AuthService } from '../auth/auth.service'
+
+const mockProperties: IProperty[] = [
+  {
+    title: 'House in Paris',
+    description: 'A beautiful house in Paris',
+    property_catergory: 'House',
+    address: '1, rue de la paix',
+    city: 'Paris',
+    state: 'Ile-de-France',
+    country: 'France',
+    price: 100,
+    surface_area: 100,
+    floors: 1,
+    capacity: 2,
+    owner_email: 'zain.zafar@gmail.com',
+  },
+  {
+    title: 'Apartment in Lyon',
+    description: 'A beautiful apartment in Lyon',
+    property_catergory: 'Apartment',
+    address: '1, rue General de Gaulle',
+    city: 'Lyon',
+    state: 'Auvergne-Rhône-Alpes',
+    country: 'France',
+    price: 200,
+    surface_area: 50,
+    floors: 5,
+    capacity: 4,
+    owner_email: 'abc@gmail.com'
+  }
+];
 
 @Injectable({
   providedIn: 'root'
 })
 export class PropertyService {
 
-  private properties: IProperty[] = [
-    {
-      location: 'Paris',
-      prix: 100,
-      area: 100
-    },
-    {
-      location: 'Lyon',
-      prix: 200,
-      area: 50
-    }
-  ];
+  private properties: IProperty[] = mockProperties;
 
   private propertySubject: BehaviorSubject<IProperty[]> = new BehaviorSubject<IProperty[]>([]);
 
