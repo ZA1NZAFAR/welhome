280a90c5526a981ddb85b88e3cb3def5f2cb38e5
afkbeto <qv.nguyen00@gmail.com>
2023-04-01 21:22:33 +0200

    models

--- FrontEnd/src/app/core/auth/auth.model.ts
+++ FrontEnd/src/app/core/auth/auth.model.ts
@@ -11,3 +11,16 @@ export interface IProfile {
 }
 
 export type ITokenPayload = IProfile & JwtPayload;
+
+/**
+ * CREATE TABLE profile
+(
+    email             VARCHAR(100) UNIQUE NOT NULL PRIMARY KEY,
+    first_name        VARCHAR(50)         NOT NULL,
+    last_name         VARCHAR(50)         NOT NULL,
+    birth_date        DATE                NOT NULL,
+    phone_number      VARCHAR(20)         NOT NULL,
+    gender            VARCHAR(20)         NOT NULL CHECK (gender IN ('Male', 'Female', 'Non-Binary')),
+    registration_date DATE DEFAULT CURRENT_DATE
+);
+ */
--- FrontEnd/src/app/core/property/property.model.ts
+++ FrontEnd/src/app/core/property/property.model.ts
@@ -17,3 +17,26 @@ export interface IProperty {
   image_url?: string
 
 }
+
+/**
+ * CREATE TABLE property
+(
+    id                BIGSERIAL PRIMARY KEY,
+    title             VARCHAR(200)   NOT NULL,
+    description       VARCHAR(500)   NOT NULL,
+    property_category VARCHAR(100)   NOT NULL CHECK (property_category IN ('House', 'Apartment', 'Room')),
+    address           VARCHAR(200)   NOT NULL,
+    city              VARCHAR(50)    NOT NULL,
+    state             VARCHAR(50),
+    country           VARCHAR(50)    NOT NULL,
+    price             NUMERIC(10, 2) NOT NULL CHECK (price > 0),
+    surface_area      REAL           NOT NULL CHECK (surface_area > 0),
+    floors            INT            NOT NULL,
+    capacity          INT            NOT NULL CHECK (capacity > 0),
+    construction_date DATE CHECK (construction_date <= CURRENT_DATE),
+    publish_date      DATE DEFAULT CURRENT_DATE,
+    owner_email       VARCHAR(100)   NOT NULL,
+    image_url         VARCHAR(1500),
+    CONSTRAINT property_email_fk FOREIGN KEY (owner_email) REFERENCES profile (email) ON DELETE CASCADE
+);
+ */
