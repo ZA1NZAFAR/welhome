134d9816d12b08b641a0412c4396f8986d93bc81
afkbeto <qv.nguyen00@gmail.com>
2023-04-02 17:00:33 +0200

    init property form

--- FrontEnd/src/app/host-property-list/host-property-list.module.ts
+++ FrontEnd/src/app/host-property-list/host-property-list.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HostPropertyListComponent } from './host-property-list.component';
 import { HostPropertyCardComponent } from './host-property-card/host-property-card.component';
+import { PropertyFormComponent } from './property-form/property-form.component';
 
 
 
 @NgModule({
   declarations: [
     HostPropertyListComponent,
-    HostPropertyCardComponent
+    HostPropertyCardComponent,
+    PropertyFormComponent
   ],
   imports: [
     CommonModule
