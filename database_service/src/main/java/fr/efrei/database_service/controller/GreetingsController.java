2cb7d85884426fb1ba5169108f316e0bf316285e
ZainZafar <zainzafarsial@gmail.com>
2023-04-02 02:28:35 +0200

    swagger example

--- database_service/src/main/java/fr/efrei/database_service/controller/GreetingsController.java
+++ database_service/src/main/java/fr/efrei/database_service/controller/GreetingsController.java
@@ -1,13 +1,17 @@
 package fr.efrei.database_service.controller;
 
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Tag(name = "Greetings", description = "You want to say hi ?")
 public class GreetingsController {
 
     @GetMapping("/greetings")
+    @Operation(summary = "Say hi to the world")
     public String getGreetings() {
         return "Hiiiii \uD83E\uDEF6 \uD83E\uDEF0 !";
     }
