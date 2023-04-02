4b5bbc89b47946a33e4725fcbee12ccb20677718
malak <malak.zemrani@efrei.net>
2023-04-02 14:11:42 +0200

    comment endpoint reservation & review

--- database_service/src/main/java/fr/efrei/database_service/controller/ReservationController.java
+++ database_service/src/main/java/fr/efrei/database_service/controller/ReservationController.java
@@ -4,6 +4,8 @@ import fr.efrei.database_service.dto.ReservationDTO;
 import fr.efrei.database_service.entity.ReservationEntity;
 import fr.efrei.database_service.service.ReservationService;
 import fr.efrei.database_service.tools.Mapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,8 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Tag(name = "Reservation", description = "This API will allow to carry out different operations on the reservations made by the renters.")
+
 @RequestMapping("/api/reservations")
 public class ReservationController {
 
@@ -19,54 +23,65 @@ public class ReservationController {
     private ReservationService reservationService;
 
     @GetMapping
+    @Operation(summary = "This endPoint will allow to retrieve all reservations made by renters")
     public List<ReservationDTO> getAllReservations() {
         return this.reservationService.findAll().stream().map(property -> Mapper.convertToDto(property, ReservationDTO.class)).collect(Collectors.toList());
     }
     @PostMapping
+    @Operation(summary = "This endPoint will allow to create a new booking for a specific renter ")
     public ReservationDTO createReservation(@RequestBody ReservationEntity reservation) {
         return Mapper.convertToDto(this.reservationService.save(reservation), ReservationDTO.class);
 
     }
     @GetMapping("/{reservationId}")
+    @Operation(summary = "This endPoint will allow to display one of the bookings by providing the relevant booking id ")
     public ReservationDTO getReservationId(@PathVariable long reservationId) {
         return Mapper.convertToDto(this.reservationService.findById(reservationId), ReservationDTO.class);
 
     }
     @GetMapping("/propertyId/{propertyId}")
+    @Operation(summary = "This endPoint will allow to display all bookings with the relevant property id ")
     public List<ReservationDTO> getPropertyId(@PathVariable long propertyId) {
         return this.reservationService.findByPropertyId(propertyId).stream().map(property -> Mapper.convertToDto(property, ReservationDTO.class)).collect(Collectors.toList());
     }
     @GetMapping("/renterEmail/{renterEmail}")
+    @Operation(summary = "This endPoint will allow to display all bookings made by a person, by providing an e-mail address ")
     public List<ReservationDTO> getRenterEmail(@PathVariable String renterEmail) {
         return this.reservationService.findByRenterEmail(renterEmail).stream().map(property -> Mapper.convertToDto(property, ReservationDTO.class)).collect(Collectors.toList());
 
     }
     @GetMapping("/startDate/{startDate}")
+    @Operation(summary = "This endPoint will allow to display all bookings made on a specific start date ")
     public List<ReservationDTO> getStartDate(@PathVariable Date startDate) {
         return this.reservationService.findByStartDate(startDate).stream().map(property -> Mapper.convertToDto(property, ReservationDTO.class)).collect(Collectors.toList());
 
     }
     @GetMapping("/endDate/{endDate}")
+    @Operation(summary = "This endPoint will allow to display all bookings made on a specific end date ")
     public List<ReservationDTO> getEndDate(@PathVariable Date endDate) {
         return this.reservationService.findByEndDate(endDate).stream().map(property -> Mapper.convertToDto(property, ReservationDTO.class)).collect(Collectors.toList());
 
     }
     @GetMapping("/confirmedOwner/{confirmedOwner}")
+    @Operation(summary = "This endPoint will allow to know if the owner has confirmed the reservation")
     public List<ReservationDTO> getConfirmedOwner(@PathVariable boolean confirmedOwner) {
         return this.reservationService.findByConfirmedOwner(confirmedOwner).stream().map(property -> Mapper.convertToDto(property, ReservationDTO.class)).collect(Collectors.toList());
 
     }
     @GetMapping("/confirmedRenter/{confirmedRenter}")
+    @Operation(summary = "This endPoint will allow to know if the renter has confirmed the reservation")
     public List<ReservationDTO> getConfirmedRenter(@PathVariable boolean confirmedRenter) {
         return this.reservationService.findByConfirmedRenter(confirmedRenter).stream().map(property -> Mapper.convertToDto(property, ReservationDTO.class)).collect(Collectors.toList());
 
     }
     @PutMapping("/{reservationId}")
+    @Operation(summary = "This endPoint will allow to update a booking by providing the relevant booking id")
     public ReservationDTO updateReservation(@PathVariable long reservationId, @RequestBody ReservationEntity reservation) {
         return Mapper.convertToDto(reservationService.update(reservationId, reservation), ReservationDTO.class);
 
     }
     @DeleteMapping("/{reservationId}")
+    @Operation(summary = "This endPoint will allow to delete a booking by providing the relevant booking id")
     public void deleteReservation(@PathVariable long reservationId) {
         this.reservationService.deleteById(reservationId);
     }
--- database_service/src/main/java/fr/efrei/database_service/controller/ReviewController.java
+++ database_service/src/main/java/fr/efrei/database_service/controller/ReviewController.java
@@ -4,6 +4,8 @@ import fr.efrei.database_service.dto.ReviewDTO;
 import fr.efrei.database_service.entity.ReviewEntity;
 import fr.efrei.database_service.service.ReviewService;
 import fr.efrei.database_service.tools.Mapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,6 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@Tag(name = "Review", description = "This API will allow to give an opinion on a specific reservation made by the renters.")
 @RequestMapping("/api/reviews")
 public class ReviewController {
 
@@ -19,51 +22,60 @@ public class ReviewController {
     private ReviewService reviewService;
 
     @PostMapping
+    @Operation(summary = "This endPoint will allow to create a specific review about a reservation")
     public ReviewDTO createReview(@RequestBody ReviewEntity reviewEntity) {
         return Mapper.convertToDto(reviewService.save(reviewEntity), ReviewDTO.class);
     }
     @GetMapping("/{id}")
+    @Operation(summary = "This endPoint will allow to display a specific review about a reservation by providing its id")
     public ReviewDTO getReview(@PathVariable long id) {
         return Mapper.convertToDto(reviewService.findById(id), ReviewDTO.class);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "This endPoint will allow to update a specific review about a reservation by providing its id")
     public ReviewDTO updateReview(@PathVariable long id, @RequestBody ReviewEntity reviewEntity) {
         return Mapper.convertToDto(reviewService.update(id, reviewEntity), ReviewDTO.class);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "This endPoint will allow to delete a specific review about a reservation by providing its id")
     public void deleteReview(@PathVariable long id) {
         this.reviewService.deleteById(id);
     }
 
     @GetMapping
-    public List<ReviewDTO> getAllUsers() {
+    @Operation(summary = "This endPoint will allow to display all reviews of users")
+    public List<ReviewDTO> getAllReviews() {
         return this.reviewService.findAll().stream().map(reviewEntity -> Mapper.convertToDto(reviewEntity, ReviewDTO.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/rating/{rating}")
+    @Operation(summary = "This endPoint will allow to display all reviews of a specific rating number")
     public List<ReviewDTO> getRating(@PathVariable int rating){
         return this.reviewService.findByRating(rating).stream().map(review -> Mapper.convertToDto(review, ReviewDTO.class)).collect(Collectors.toList());
     }
     @GetMapping("/comment/{text}")
+    @Operation(summary = "This endPoint will allow to display all reviews of a specific comment")
     public List<ReviewDTO> getReviewText(@PathVariable String text){
         return this.reviewService.findByReviewText(text).stream().map(review -> Mapper.convertToDto(review, ReviewDTO.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/publish_date/{date}")
+    @Operation(summary = "This endPoint will allow to retrieve all reviews of a specific publish date")
     public List<ReviewDTO> getPublishDate(@PathVariable Date date){
         return this.reviewService.findByPublishDate(date).stream().map(review -> Mapper.convertToDto(review, ReviewDTO.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/property_id/{id}")
+    @Operation(summary = "This endPoint will allow to retrieve all reviews of a specific property id")
     public List<ReviewDTO> getPropertyId(@PathVariable int id){
         return this.reviewService.findByPropertyId(id).stream().map(review -> Mapper.convertToDto(review, ReviewDTO.class)).collect(Collectors.toList());
     }
 
     @GetMapping("/reviewer_email/{email}")
+    @Operation(summary = "This endPoint will allow to retrieve all reviews made by a specific reviewer")
     public List<ReviewDTO> getReviewerEmail(@PathVariable String email){
         return this.reviewService.findByReviewerEmail(email).stream().map(review -> Mapper.convertToDto(review, ReviewDTO.class)).collect(Collectors.toList());
     }
-
 }
