96c9fd6135b619bec0d1fa73ef418567f015d53b
Rimberse <kerimpltm@gmail.com>
2023-04-03 23:50:42 +0200

    Use ParameterizedTypeReference for generic types of parameter

--- backend/src/main/java/fr/efrei/backend/controllers/ProfilesController.java
+++ backend/src/main/java/fr/efrei/backend/controllers/ProfilesController.java
@@ -3,10 +3,15 @@ package fr.efrei.backend.controllers;
 import fr.efrei.backend.entities.Profile;
 import fr.efrei.backend.utils.ResponseGenerator;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @RestController
 public class ProfilesController {
@@ -16,14 +21,14 @@ public class ProfilesController {
     @GetMapping("/api/profiles")
     public ResponseEntity<Profile[]> all() {
         ResponseGenerator<Profile[]> generator = new ResponseGenerator<>();
-        ResponseEntity<Profile[]> result = generator.execute(URL, HttpMethod.GET, Profile[].class);
+        ResponseEntity<Profile[]> result = generator.execute(URL, HttpMethod.GET, new ParameterizedTypeReference<Profile[]>() {});
         return result;
     }
 
     @GetMapping("/api/profiles/{email}")
     public ResponseEntity<Profile> one(@PathVariable String email) {
         ResponseGenerator<Profile> generator = new ResponseGenerator<>();
-        ResponseEntity<Profile> result = generator.execute(URL.concat("/" + email), HttpMethod.GET, Profile.class);
+        ResponseEntity<Profile> result = generator.execute(URL.concat("/" + email), HttpMethod.GET, new ParameterizedTypeReference<Profile>() {});
         return result;
     }
 }
--- backend/src/main/java/fr/efrei/backend/utils/ResponseGenerator.java
+++ backend/src/main/java/fr/efrei/backend/utils/ResponseGenerator.java
@@ -1,8 +1,7 @@
 package fr.efrei.backend.utils;
 
-import fr.efrei.backend.entities.Profile;
-import lombok.extern.java.Log;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,7 +9,7 @@ import java.util.Collections;
 
 @Slf4j
 public class ResponseGenerator<T> {
-    public ResponseEntity<T> execute(String requestUrl, HttpMethod method, Class<T> dto) {
+    public ResponseEntity<T> execute(String requestUrl, HttpMethod method, ParameterizedTypeReference<T> responseType) {
         // Create RestTemplate instance
         RestTemplate restTemplate = new RestTemplate();
         log.info("Sending HTTP " + method.name() + " request to: " + requestUrl);
@@ -21,7 +20,7 @@ public class ResponseGenerator<T> {
 
         // Create HttpEntity, execute Http Request, create ResponseEntity serialize JSON body
         HttpEntity<String> entity = new HttpEntity<>("parameters", headers);
-        ResponseEntity<T> result = restTemplate.exchange(requestUrl, method, entity, dto);
+        ResponseEntity<T> result = restTemplate.exchange(requestUrl, method, entity, responseType);
         return result;
     }
 }
