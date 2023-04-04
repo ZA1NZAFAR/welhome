e65955e7f38e7a671ae5f0b349446882257d9a8a
jess7 <jess77500@gmail.com>
2023-04-05 00:10:05 +0200

    "pushing everything"

--- Authentication/backend/db.js
+++ Authentication/backend/db.js
@@ -4,7 +4,12 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  //connectionString: process.env.DATABASE_URL,
 });
 
 module.exports = {
