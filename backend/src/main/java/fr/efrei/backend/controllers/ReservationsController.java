873bcfcfeeb139a5f74329971fa4d49d932e0c39
Rimberse <kerimpltm@gmail.com>
2023-04-05 21:36:48 +0200

    Add GET endpoints to retrieve all reservation, reservation by ID, reservation by property ID & reservation by renter email

--- backend/src/main/java/fr/efrei/backend/controllers/ReservationsController.java
+++ backend/src/main/java/fr/efrei/backend/controllers/ReservationsController.java
@@ -1,11 +1,17 @@
 package fr.efrei.backend.controllers;
 
+import fr.efrei.backend.entities.Property;
 import fr.efrei.backend.entities.Reservation;
 import fr.efrei.backend.entities.Review;
 import fr.efrei.backend.utils.ResponseGenerator;
 import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,5 +34,27 @@ public class ReservationsController {
         generator = new ResponseGenerator<>();
     }
 
-    
+    @GetMapping
+    public ResponseEntity<List<Reservation>> getReservations() {
+        ResponseEntity<List<Reservation>> result = listGenerator.buildRequest(URL, HttpMethod.GET, new ParameterizedTypeReference<List<Reservation>>() {});
+        return result;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Reservation> getReservation(@PathVariable String id) {
+        ResponseEntity<Reservation> result = generator.buildRequest(URL.concat("/" + id), HttpMethod.GET, new ParameterizedTypeReference<Reservation>() {});
+        return result;
+    }
+
+    @GetMapping("/property_id/{propertyId}")
+    public ResponseEntity<List<Reservation>> getPropertyByPropertyId(@PathVariable String propertyId) {
+        ResponseEntity<List<Reservation>> result = listGenerator.buildRequest(URL.concat("/propertyId/" + propertyId), HttpMethod.GET, new ParameterizedTypeReference<List<Reservation>>() {});
+        return result;
+    }
+
+    @GetMapping("/renter_email/{renterEmail}")
+    public ResponseEntity<List<Reservation>> getPropertyByRenterEmail(@PathVariable String renterEmail) {
+        ResponseEntity<List<Reservation>> result = listGenerator.buildRequest(URL.concat("/renterEmail/" + renterEmail), HttpMethod.GET, new ParameterizedTypeReference<List<Reservation>>() {});
+        return result;
+    }
 }
