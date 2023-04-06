e79245fbaa85092fe1a3f536b72e3d0cf39c499b
QV NGUYEN <qv.nguyen00@gmail.com>
2023-04-06 13:19:23 +0200

    fix properties

--- FrontEnd/src/app/properties/properties.component.ts
+++ FrontEnd/src/app/properties/properties.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { IProperty } from 'src/app/core/property/property.model';
@@ -9,8 +9,11 @@ import { PropertyService } from 'src/app/core/property/property.service';
   templateUrl: './properties.component.html',
   styleUrls: ['./properties.component.scss']
 })
-export class PropertiesComponent implements OnInit {
+export class PropertiesComponent implements OnInit, OnDestroy {
   propertyData: IProperty;
+
+  getPropertySub$: Subscription;
+  paramsSub$: Subscription;
 
   constructor(
     private propertyService: PropertyService,
@@ -19,13 +22,13 @@ export class PropertiesComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe(params => {
+    this.paramsSub$ = this.route.paramMap.subscribe(params => {
       const id = Number(params.get('id'));
       if (id === null) {
         this.router.navigate(['properties']);
         return;
       }
-      this.propertyService.getProperties().subscribe(properties => {
+      this.getPropertySub$ = this.propertyService.getProperties().subscribe(properties => {
         const property = properties.find(p => p.id === id);
         if (property === undefined) {
           this.router.navigate(['properties']);
@@ -36,5 +39,45 @@ export class PropertiesComponent implements OnInit {
     });
   }
 
+  ngOnDestroy(): void {
+    this.getPropertySub$.unsubscribe();
+    this.paramsSub$.unsubscribe();
+  }
 
+  deleteProperty(): void {
+    this.propertyService.deleteProperty(this.propertyData.id).subscribe(() => {
+      this.router.navigate(['properties']);
+    });
+  }
+
+  nextImage(): void {
+
+  }
+  /*
+  $(document).ready(function() {
+    var currentPhoto = 1;
+    var numPhotos = $('.small-photo').length;
+
+    function showPhoto(photoNum) {
+      $('.main-photo').attr('src', $('.small-photo:eq(' + (photoNum-1) + ')').attr('src'));
+      $('.small-photo').removeClass('active');
+      $('.small-photo:eq(' + (photoNum-1) + ')').addClass('active');
+    }
+
+    $('.small-photo').click(function() {
+      currentPhoto = $(this).index() + 1;
+      showPhoto(currentPhoto);
+    });
+
+    $('#next-btn').click(function() {
+      currentPhoto++;
+      if (currentPhoto > numPhotos) {
+        currentPhoto = 1;
+      }
+      showPhoto(currentPhoto);
+    });
+
+    showPhoto(currentPhoto);
+  });
+  */
 }
