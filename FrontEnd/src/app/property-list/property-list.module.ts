ba2bdea464ba0282ada04dd1685cd86a0289b85a
Euphraïm LUZOLO <euphraim.luzolo@efrei.net>
2023-04-08 14:57:37 +0200

    changes

--- FrontEnd/src/app/property-list/property-list.module.ts
+++ FrontEnd/src/app/property-list/property-list.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SearchBarComponent } from './search-bar/search-bar.component';
 import { PropertyListComponent } from './property-list.component';
 import { PropertyCardComponent } from './property-card/property-card.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
     SearchBarComponent,
     PropertyListComponent,
-    PropertyCardComponent
+    PropertyCardComponent,
+    FilterComponent
   ],
   imports: [
     CommonModule
