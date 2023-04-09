2df2f3b3a2a1a7c3c58a59e0800aad9c47043e12
afkbeto <qv.nguyen00@gmail.com>
2023-04-09 12:16:49 +0200

    add json MIME

--- FrontEnd/src/app/core/auth/auth.interceptor.ts
+++ FrontEnd/src/app/core/auth/auth.interceptor.ts
@@ -1,7 +1,7 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { AuthService } from "./auth.service";
-import { Observable } from "rxjs";
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -13,7 +13,7 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const authToken = this.authService.token;
     const authRequest = request.clone({
-      headers: request.headers.set("Authorization", "Bearer " + authToken)
+      headers: request.headers.set('Authorization', 'Bearer ' + authToken).set('Content-Type', 'application/json'),
     });
     return next.handle(authRequest);
   }
