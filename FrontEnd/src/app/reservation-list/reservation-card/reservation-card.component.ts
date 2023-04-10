30d323a4fb6bbf174dfad4ee156d36b5b1d57c65
jess7 <jess77500@gmail.com>
2023-04-10 15:41:08 +0200

    Add review button and review form done.

--- FrontEnd/src/app/reservation-list/reservation-card/reservation-card.component.ts
+++ FrontEnd/src/app/reservation-list/reservation-card/reservation-card.component.ts
@@ -3,8 +3,18 @@ import { IProperty } from 'src/app/core/property/property.model'
 import { PropertyService } from 'src/app/core/property/property.service'
 import { IReservation } from 'src/app/core/reservation/reservation.model';
 import { ReservationService } from 'src/app/core/reservation/reservation.service'
+<<<<<<< Updated upstream
 import { ActivatedRoute } from '@angular/router';
 
+=======
+import { ActivatedRoute, Router } from '@angular/router';
+import { ReviewService } from 'src/app/core/review/review.service';
+import { AuthService } from 'src/app/core/auth/auth.service';
+import { ContextService } from 'src/app/core/context/context.service';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import {ReviewFormComponent} from "../../review-form/review-form.component";
+import { ReactiveFormsModule } from '@angular/forms';
+>>>>>>> Stashed changes
 
 @Component({
   selector: 'app-reservation-card',
@@ -19,6 +29,7 @@ export class ReservationCardComponent implements OnInit {
   constructor(
     private propertyService: PropertyService,
     private reservationService: ReservationService,
+<<<<<<< Updated upstream
     private route: ActivatedRoute
   ) { }
 
@@ -27,6 +38,38 @@ export class ReservationCardComponent implements OnInit {
       this.propertyId = params['propertyId'];
       console.log("ici");
     });
+=======
+    private router: Router,
+    private reviewService: ReviewService,
+    private authService: AuthService,
+    private contextService: ContextService,
+    private modalService: NgbModal
+  ) { }
+
+  ngOnInit() {
+    const email = this.contextService.isRenter ? this.authService.profile!.email : this.reservation.renter_email;
+    const reviewSub$ = this.reviewService.getPropertyReviews(this.property.id).subscribe(reviews => {
+      const review = reviews.find(r => r.reviewer_email === email);
+      if (review) {
+        this.rating = review.rating;
+      }
+      reviewSub$.unsubscribe();
+    });
+  }
+
+  get ratingText(): string {
+    if (this.rating === -1) {
+      return 'No rating';
+    }
+    return this.rating.toString();
+  }
+
+  get ratingDescription(): string {
+    if (this.contextService.isRenter) {
+      return 'Your rating';
+    }
+    return 'Renter rating';
+>>>>>>> Stashed changes
   }
 
   get status(): string {
@@ -65,5 +108,21 @@ export class ReservationCardComponent implements OnInit {
     }
   }
 
+<<<<<<< Updated upstream
+=======
+  goToProperty() {
+    this.router.navigate(['properties', this.property.id]);
+  }
+
+  openReviewForm(propertyId: number) {
+    const modalRef = this.modalService.open(ReviewFormComponent, { centered: true });
+    modalRef.componentInstance.propertyId = propertyId;
+    modalRef.result.then((result) => {
+      // Do something with the result if needed
+    }, (reason) => {
+      // Handle the modal dismissal if needed
+    });
+  }
+>>>>>>> Stashed changes
 
 }
