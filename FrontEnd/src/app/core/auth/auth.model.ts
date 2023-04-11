d5440063ba85a64b5b0fdd5a46746938e8d617b4
afkbeto <qv.nguyen00@gmail.com>
2023-04-11 13:24:06 +0200

    temporary tweak for auth profile

--- FrontEnd/src/app/core/auth/auth.model.ts
+++ FrontEnd/src/app/core/auth/auth.model.ts
@@ -2,11 +2,11 @@ import { JwtPayload } from 'jwt-decode'
 
 export interface IProfile {
   email: string;
-  first_name: string;
-  last_name: string;
-  birth_date: Date;
-  phone_number: string;
-  gender: 'Male' | 'Female' | 'Non-Binary'
+  first_name?: string;
+  last_name?: string;
+  birth_date?: Date;
+  phone_number?: string;
+  gender?: 'Male' | 'Female' | 'Non-Binary'
   registration_date?: Date;
 }
 
