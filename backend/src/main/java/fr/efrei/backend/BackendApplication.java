3ba20b398de90a4f8b722f8ca38de4ca91ab72e2
Rimberse <kerimpltm@gmail.com>
2023-04-12 18:23:50 +0200

    Create AccessTokenValidator utility class, used to verify if Authorization token is valid or not

--- backend/src/main/java/fr/efrei/backend/BackendApplication.java
+++ backend/src/main/java/fr/efrei/backend/BackendApplication.java
@@ -2,7 +2,9 @@ package fr.efrei.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -20,5 +22,10 @@ public class BackendApplication {
 				registry.addMapping("/*").allowedOrigins("*");
 			}
 		};
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
 	}
 }
---
+++ backend/src/main/java/fr/efrei/backend/utils/AccessTokenValidator.java
@@ -0,0 +1,47 @@
+package fr.efrei.backend.utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.http.HttpResponse;
+import java.util.Optional;
+
+@Component
+public class AccessTokenValidator {
+    private static RestTemplate restTemplate = new RestTemplate();
+
+    private static HttpHeaders getRequestHeaderBearer(String bearerAuthorizationToken) {
+        HttpHeaders headers = new HttpHeaders();
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBearerAuth(bearerAuthorizationToken);
+
+        return headers;
+    }
+
+    public static boolean isTokenValid(String url, String accessToken) {
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.POST, new HttpEntity<>(null, getRequestHeaderBearer(accessToken)), String.class);
+        final String message = "Token is valid";
+
+        // Parse response JSON data
+        try {
+            JSONObject jsonObject = new JSONObject(response.getBody());
+            System.out.println(jsonObject);
+            System.out.println(jsonObject.get("message"));
+            return jsonObject.get("message").equals(message);
+        } catch (JSONException exception) {
+            System.out.println("Couldn't parse JSON: " + exception.getMessage());
+
+            if (response.getBody() == null)
+                return false;
+            else {
+                String value = response.getBody().split(":")[1];
+                return value.substring(1, value.length() - 2).equals(message);
+            }
+        }
+    }
+}
